326804e1833de8f5074c23240c749f31b414f112
Enel Almonte <enel_almonte@ucne.edu.do>
2023-05-06 17:48:06 -0400

    Agregando el Modelo Sistemas

---
+++ Models/Sistemas.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TicketManager.Models
+{
+    public class Sistemas
+    {
+        [Key]
+        public int SistemaId { get; set; }
+
+        [Required (ErrorMessage ="El nombre del sistema es obligatorio")]
+        public String? Nombre { get; set; }
+    }
+}
